6cdaa27c0987711ae07e32f43a0c758dab591439
injeong <injong032@gmail.com>
2020-06-04 02:19:25 +0900

    등록API, 테스트코드 추가

---
+++ src/main/java/com/injeong/book/springboot/service/PostsService.java
@@ -0,0 +1,20 @@
+package com.injeong.book.springboot.service;
+
+import com.injeong.book.springboot.domain.posts.PostsRepository;
+import com.injeong.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
